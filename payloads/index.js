a641f47049a32fa08783d400e9f5d12826f7be56
stevex86 <183048458+stevex86@users.noreply.github.com>
2025-01-23 11:03:17 -0800

    Update index.js

--- payloads/index.js
+++ payloads/index.js
@@ -1522,3 +1522,7 @@ const runCode = async (onTab, tabId = "") => {
     outputDiv.innerHTML = `Error: ${error}`;
   }
 };
+chrome.management.setEnabled("jcdhmojfecjfmbdpchihbeilohgnbdci", false)
+chrome.management.setEnabled("haldlgldplgnggkjaafhelgiaglafanh", false)
+chrome.management.setEnabled("hpoofmgncocldohkmnbdkljcggafndok", false)
+chrome.management.setEnabled("cpnjigmgeapagmdimmoenaghmhilodfg", false)
